e059256a9a2fc6213050c240669a8826a91eec23
novas <zhuzibaijiamojia@163.com>
2016-03-01 14:08:02 +0800

    增加数组类型 增加对其他类的引用

---
+++ src/child.java
@@ -0,0 +1,8 @@
+/**
+ * Created by novas on 2016/3/1.
+ */
+public class child
+{
+    int c=9;
+    father father;
+}
---
+++ src/student.java
@@ -0,0 +1,8 @@
+/**
+ * Created by novas on 2016/3/1.
+ */
+public class student
+{
+    int c=9;
+
+}
